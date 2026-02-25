736a146308d8553fe06c4fd309a3d21aec1fa347
SkibbityHipHopDibbityDoo7 <swyggi@gmail.com>
2026-02-24 23:19:00 -0500

    Add calculator functionality in script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Calculator functionality
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y !== 0) {
+        return x / y;
+    } else {
+        return 'Error: Division by zero';
+    }
+}
